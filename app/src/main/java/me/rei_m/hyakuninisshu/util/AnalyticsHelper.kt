a4ffa63b2abe326079846d4580d3fcbe6aea6b0e
rei-m <star.oceans.rm@gmail.com>
2018-08-15 02:31:27 +0900

    add sendscreent to analyticshelper

--- app/src/main/java/me/rei_m/hyakuninisshu/util/AnalyticsHelper.kt
+++ app/src/main/java/me/rei_m/hyakuninisshu/util/AnalyticsHelper.kt
@@ -13,6 +13,7 @@
 
 package me.rei_m.hyakuninisshu.util
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 
@@ -26,27 +27,42 @@ class AnalyticsHelper @Inject constructor(context: Context) {
 
     private val analytics = FirebaseAnalytics.getInstance(context)
 
-    fun logActionEvent(event: ActionEvent) {
-        val bundle = Bundle().apply {
-            putString(FirebaseAnalytics.Param.ITEM_ID, event.id)
-            putString(FirebaseAnalytics.Param.ITEM_NAME, event.eventName)
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, event.contentType)
+    fun sendScreenView(screenName: String, activity: Activity) {
+        val params = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_ID, screenName)
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, CONTENT_TYPE_SCREEN_VIEW)
         }
-        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        analytics.run {
+            setCurrentScreen(activity, screenName, null)
+            logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, params)
+        }
+        Logger.analytics("Screen($screenName) recorded")
     }
 
-    enum class ActionEvent(val id: String,
-                           val eventName: String,
-                           val contentType: String) {
-        START_TRAINING("10", "start_training", CONTENT_TYPE_BUTTON),
-        START_EXAM("11", "start_exam", CONTENT_TYPE_BUTTON),
-        START_TRAINING_FOR_EXAM("12", "start_training_for_exam", CONTENT_TYPE_BUTTON),
-        RESTART_TRAINING("13", "restart_training", CONTENT_TYPE_BUTTON),
-        FINISH_TRAINING("14", "finish_training", CONTENT_TYPE_BUTTON),
-        FINISH_EXAM("15", "finish_exam", CONTENT_TYPE_BUTTON)
+    fun logActionEvent(event: ActionEvent) {
+        val params = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_ID, event.id)
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, CONTENT_TYPE_ACTION_EVENT)
+            putString(KEY_ACTION, event.event)
+        }
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, params)
+        Logger.analytics("$event recorded")
+    }
+
+    enum class ActionEvent(val id: String, val event: String) {
+        START_TRAINING("10", "start_training"),
+        START_EXAM("11", "start_exam"),
+        START_TRAINING_FOR_EXAM("12", "start_training_for_exam"),
+        RESTART_TRAINING("13", "restart_training"),
+        FINISH_TRAINING("14", "finish_training"),
+        FINISH_EXAM("15", "finish_exam");
+
+        override fun toString(): String = "ActionEvent(id='$id', event='$event')"
     }
 
     companion object {
-        private const val CONTENT_TYPE_BUTTON = "button"
-    }
+        private const val KEY_ACTION = "ui_action"
+        private const val CONTENT_TYPE_SCREEN_VIEW = "screen"
+        private const val CONTENT_TYPE_ACTION_EVENT = "action event"
+     }
 }
